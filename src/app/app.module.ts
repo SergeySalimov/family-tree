1a98f9ae7b8410c6283b88fb73acd4d456c4a7cc
SergeySalimov <salimov_s@tut.by>
2021-02-07 19:07:52 +0300

    FT-001 linked to firebase, added auto deploy on each new PR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+      apiKey: 'AIzaSyAhCPN1xulD6YsjjieAaEZFMHZIytPeHB0',
+      authDomain: 'family-tree-s.firebaseapp.com',
+      databaseURL: 'https://family-tree-s-default-rtdb.europe-west1.firebasedatabase.app',
+      projectId: 'family-tree-s',
+      storageBucket: 'family-tree-s.appspot.com',
+      messagingSenderId: '577412796018',
+      appId: '1:577412796018:web:a4cdf66a88335ec71d3ceb',
+      measurementId: 'G-0195EP6VVH',
+  },
 };
 
 /*
